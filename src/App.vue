4ee09ebead65c93adcddfc16eac1056ecd8ba1d2
TOP-Z\riley <riley211375@gmail.com>
2023-09-02 20:16:44 +0800

    import on demand

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup></script>
 
 <template>
-  <div>app</div>
+  <div>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <style scoped></style>
